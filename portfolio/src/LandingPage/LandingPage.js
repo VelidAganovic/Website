b79db4ca2374907f3f69f00f440462c3f3a77eb7
Velid Aganovic <velid.aganovic@gmail.com>
2021-05-26 22:33:23 +0200

    fix landing page

--- portfolio/src/LandingPage/LandingPage.js
+++ portfolio/src/LandingPage/LandingPage.js
@@ -2,21 +2,18 @@ import React from 'react';
 import './LandingPage.css';
 
 function LandingPage(props) {
+    // size is amount of drops also needs to be adjusted in css
+    const size = 20;
+    var indents = [];
+    for (var i = 0; i < size; i++) {
+    indents.push(<div class="line"></div>);
+    }
     return (
       <div className="App">
           <div class="lines">
-            <div class="line"></div>
-            <div class="line"></div>
-            <div class="line"></div>
-            <div class="line"></div>
-            <div class="line"></div>
-            <div class="line"></div>
-            <div class="line"></div>
-            <div class="line"></div>
-            <div class="line"></div>
-            <div class="line"></div>
-            <div class="line"></div>
+            {indents}
         </div>
+        <h1>Hi! I'm Velid Aganovic, a Full-Stack Web Developer</h1> 
       </div>
     )
   }
